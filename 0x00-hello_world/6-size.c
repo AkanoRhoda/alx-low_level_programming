e930e25b15bb7d3d50494e6de057efd351ab3503
AkanoRhoda <akanoati9@gmail.com>
2022-12-10 11:43:01 -0800

    c-programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - for the description
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of char is=%i byte\n", sizeof(char));
+	printf("size of int is=%i byte\n", sizeof(int));
+	printf("size of signed char is=%i byte\n", sizeof(signed char));
+	printf("size of unsigned char is=%i byte\n", sizeof(unsigned char));
+	printf("Size of a long int is=%i byte\n", sizeof(long int));
+	printf("Size of a long long int is=%i byte\n", sizeof(long long int));
+	printf("Size of a float is=%i byte\n", sizeof(float));
+
+	return (0);
+}
