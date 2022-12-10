46ac6186b054fd538030fc47fbfa53ddd83b769f
AkanoRhoda <akanoati9@gmail.com>
2022-12-10 11:54:47 -0800

    my first c-programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	printf("size of a char: %i byte\n", sizeof(char));
-	printf("size of an int: %i byte\n", sizeof(int));
-	printf("size of a signed char: %i byte\n", sizeof(signed char));
-	printf("size of a unsigned char: %i byte\n", sizeof(unsigned char));
-	printf("Size of a long int: %i byte\n", sizeof(long int));
-	printf("Size of a long long int: %i byte\n", sizeof(long long int));
-	printf("Size of a float: %i byte\n", sizeof(float));
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a signed char: %i byte(s)\n", sizeof(signed char));
+	printf("size of a unsigned char: %i byte(s)\n", sizeof(unsigned char));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
 }
