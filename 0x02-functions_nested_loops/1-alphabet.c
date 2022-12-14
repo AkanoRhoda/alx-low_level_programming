ce6bee5439d9b7d10a01d6e08ae40952fb03b1d2
AkanoRhoda <akanoati9@gmail.com>
2022-12-14 09:48:23 -0800

    function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/* betty style doc for function main goes there */
+/**
+ *  print_alphabet - for the alpha
+ *  @print_alphabet - for the letter
+ *  main - check the code
+ *  @main - for alpha
+ *  Return: Always 0 (Success)
+ *  print_alphabet - for  beta
+ *  @print_alphabet - for the code
+ */
+int print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
