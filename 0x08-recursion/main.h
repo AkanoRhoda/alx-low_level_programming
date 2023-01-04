0da4ff94f4ef7e4b4e3dc9e51b6dcdeb06736000
AkanoRhoda <akanoati9@gmail.com>
2023-01-04 09:49:02 -0800

    print a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int  _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *S);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int help(int n, int i);
+int help_prime(int n, int m);
+int _sqrt_root(int n, int l);
+int is_palidrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int find_str(char *s, char w, int x, int z);
+int wildcmp(char *s1, char *s2);
+
+#endif /* _MAIN_H */
+
