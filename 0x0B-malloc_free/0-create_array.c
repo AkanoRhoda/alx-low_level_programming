6a3ed745ced29efb93d6ce6612adf16921446236
AkanoRhoda <akanoati9@gmail.com>
2023-01-11 09:06:13 -0800

    pointer initialization fixed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of char
+ *	and initializes it with a specific char
+ *@size: size of array to be created
+ * @c : char to fill array with
+ *
+ * Return: Returns a pointer to array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size);
+	if (size == 0 || ptr == 0)
+		return (NULL);
+
+	while (size--)
+		ptr[size] = c;
+
+	return (ptr);
+}
